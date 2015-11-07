4e4f204d678272c2f75723957d5e07356f9e44e3
oceaneseel <oceane.seel@hotmail.com>
2015-11-07 13:12:46 +0100

    1ère phase ok

---
+++ SmartCity/DataAccessLayer/DAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DAL
+    {
+        private static DataContextDALDataContext instanceDC = null;
+
+        public static DataContextDALDataContext Instance
+        {
+            get
+            {
+                if (instanceDC == null)
+                    instanceDC = new DataContextDALDataContext();
+
+                return instanceDC;
+            }
+        }
+
+        public static List<DEFAUT> SelectAllDefauts()
+        {
+            return Instance.DEFAUTs.ToList();
+        }
+
+        public static List<INTERVENTION> SelectAllDefauts()
+        {
+            return Instance.INTERVENTIONs.ToList();
+        }
+
+        public static List<PERSONNE> SelectAllDefauts()
+        {
+            return Instance.PERSONNEs.ToList();
+        }
+    }
+}
