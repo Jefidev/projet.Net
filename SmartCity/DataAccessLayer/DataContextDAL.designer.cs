ed43546f22067dadd37d6d352aca91bcb47bfdef
oceaneseel <oceane.seel@hotmail.com>
2015-12-02 10:10:20 +0100

    Problème de nullable réglé

--- SmartCity/DataAccessLayer/DataContextDAL.designer.cs
+++ SmartCity/DataAccessLayer/DataContextDAL.designer.cs
@@ -33,12 +33,12 @@ namespace DataAccessLayer
     partial void InsertDefaut(Defaut instance);
     partial void UpdateDefaut(Defaut instance);
     partial void DeleteDefaut(Defaut instance);
-    partial void InsertPersonne(Personne instance);
-    partial void UpdatePersonne(Personne instance);
-    partial void DeletePersonne(Personne instance);
     partial void InsertIntervention(Intervention instance);
     partial void UpdateIntervention(Intervention instance);
     partial void DeleteIntervention(Intervention instance);
+    partial void InsertPersonne(Personne instance);
+    partial void UpdatePersonne(Personne instance);
+    partial void DeletePersonne(Personne instance);
     #endregion
 		
 		public DataContextDALDataContext() : 
@@ -79,19 +79,19 @@ namespace DataAccessLayer
 			}
 		}
 		
-		public System.Data.Linq.Table<Personne> Personnes
-		{
-			get
-			{
-				return this.GetTable<Personne>();
-			}
-		}
-		
 		public System.Data.Linq.Table<Intervention> Interventions
 		{
 			get
 			{
 				return this.GetTable<Intervention>();
+			}
+		}
+		
+		public System.Data.Linq.Table<Personne> Personnes
+		{
+			get
+			{
+				return this.GetTable<Personne>();
 			}
 		}
 	}
@@ -152,7 +152,7 @@ namespace DataAccessLayer
 			}
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Photo", DbType="VarBinary(MAX)", UpdateCheck=UpdateCheck.Never)]
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Photo", DbType="VarBinary(MAX)", CanBeNull=true, UpdateCheck=UpdateCheck.Never)]
 		public System.Data.Linq.Binary Photo
 		{
 			get
@@ -255,6 +255,270 @@ namespace DataAccessLayer
 		{
 			this.SendPropertyChanging();
 			entity.Defaut1 = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Interventions")]
+	public partial class Intervention : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _IdIntervention;
+		
+		private string _Etat;
+		
+		private string _Commentaire;
+		
+		private System.DateTime _DateIntervention;
+		
+		private int _Defaut;
+		
+		private string _Personne;
+		
+		private EntityRef<Defaut> _Defaut1;
+		
+		private EntityRef<Personne> _Personne1;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdInterventionChanging(int value);
+    partial void OnIdInterventionChanged();
+    partial void OnEtatChanging(string value);
+    partial void OnEtatChanged();
+    partial void OnCommentaireChanging(string value);
+    partial void OnCommentaireChanged();
+    partial void OnDateInterventionChanging(System.DateTime value);
+    partial void OnDateInterventionChanged();
+    partial void OnDefautChanging(int value);
+    partial void OnDefautChanged();
+    partial void OnPersonneChanging(string value);
+    partial void OnPersonneChanged();
+    #endregion
+		
+		public Intervention()
+		{
+			this._Defaut1 = default(EntityRef<Defaut>);
+			this._Personne1 = default(EntityRef<Personne>);
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_IdIntervention", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int IdIntervention
+		{
+			get
+			{
+				return this._IdIntervention;
+			}
+			set
+			{
+				if ((this._IdIntervention != value))
+				{
+					this.OnIdInterventionChanging(value);
+					this.SendPropertyChanging();
+					this._IdIntervention = value;
+					this.SendPropertyChanged("IdIntervention");
+					this.OnIdInterventionChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Etat", DbType="VarChar(20)")]
+		public string Etat
+		{
+			get
+			{
+				return this._Etat;
+			}
+			set
+			{
+				if ((this._Etat != value))
+				{
+					this.OnEtatChanging(value);
+					this.SendPropertyChanging();
+					this._Etat = value;
+					this.SendPropertyChanged("Etat");
+					this.OnEtatChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Commentaire", DbType="VarChar(500)")]
+		public string Commentaire
+		{
+			get
+			{
+				return this._Commentaire;
+			}
+			set
+			{
+				if ((this._Commentaire != value))
+				{
+					this.OnCommentaireChanging(value);
+					this.SendPropertyChanging();
+					this._Commentaire = value;
+					this.SendPropertyChanged("Commentaire");
+					this.OnCommentaireChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_DateIntervention", DbType="Date NOT NULL")]
+		public System.DateTime DateIntervention
+		{
+			get
+			{
+				return this._DateIntervention;
+			}
+			set
+			{
+				if ((this._DateIntervention != value))
+				{
+					this.OnDateInterventionChanging(value);
+					this.SendPropertyChanging();
+					this._DateIntervention = value;
+					this.SendPropertyChanged("DateIntervention");
+					this.OnDateInterventionChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Defaut", DbType="Int NOT NULL")]
+		public int Defaut
+		{
+			get
+			{
+				return this._Defaut;
+			}
+			set
+			{
+				if ((this._Defaut != value))
+				{
+					if (this._Defaut1.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnDefautChanging(value);
+					this.SendPropertyChanging();
+					this._Defaut = value;
+					this.SendPropertyChanged("Defaut");
+					this.OnDefautChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Personne", DbType="VarChar(50)")]
+		public string Personne
+		{
+			get
+			{
+				return this._Personne;
+			}
+			set
+			{
+				if ((this._Personne != value))
+				{
+					if (this._Personne1.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnPersonneChanging(value);
+					this.SendPropertyChanging();
+					this._Personne = value;
+					this.SendPropertyChanged("Personne");
+					this.OnPersonneChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Defaut_Intervention", Storage="_Defaut1", ThisKey="Defaut", OtherKey="IdDefaut", IsForeignKey=true)]
+		public Defaut Defaut1
+		{
+			get
+			{
+				return this._Defaut1.Entity;
+			}
+			set
+			{
+				Defaut previousValue = this._Defaut1.Entity;
+				if (((previousValue != value) 
+							|| (this._Defaut1.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._Defaut1.Entity = null;
+						previousValue.Interventions.Remove(this);
+					}
+					this._Defaut1.Entity = value;
+					if ((value != null))
+					{
+						value.Interventions.Add(this);
+						this._Defaut = value.IdDefaut;
+					}
+					else
+					{
+						this._Defaut = default(int);
+					}
+					this.SendPropertyChanged("Defaut1");
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Personne_Intervention", Storage="_Personne1", ThisKey="Personne", OtherKey="Mail", IsForeignKey=true)]
+		public Personne Personne1
+		{
+			get
+			{
+				return this._Personne1.Entity;
+			}
+			set
+			{
+				Personne previousValue = this._Personne1.Entity;
+				if (((previousValue != value) 
+							|| (this._Personne1.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._Personne1.Entity = null;
+						previousValue.Interventions.Remove(this);
+					}
+					this._Personne1.Entity = value;
+					if ((value != null))
+					{
+						value.Interventions.Add(this);
+						this._Personne = value.Mail;
+					}
+					else
+					{
+						this._Personne = default(string);
+					}
+					this.SendPropertyChanged("Personne1");
+				}
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
 		}
 	}
 	
@@ -441,270 +705,6 @@ namespace DataAccessLayer
 		{
 			this.SendPropertyChanging();
 			entity.Personne1 = null;
-		}
-	}
-	
-	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Interventions")]
-	public partial class Intervention : INotifyPropertyChanging, INotifyPropertyChanged
-	{
-		
-		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
-		
-		private int _IdIntervention;
-		
-		private string _Etat;
-		
-		private string _Commentaire;
-		
-		private System.DateTime _DateIntervention;
-		
-		private int _Defaut;
-		
-		private string _Personne;
-		
-		private EntityRef<Defaut> _Defaut1;
-		
-		private EntityRef<Personne> _Personne1;
-		
-    #region Extensibility Method Definitions
-    partial void OnLoaded();
-    partial void OnValidate(System.Data.Linq.ChangeAction action);
-    partial void OnCreated();
-    partial void OnIdInterventionChanging(int value);
-    partial void OnIdInterventionChanged();
-    partial void OnEtatChanging(string value);
-    partial void OnEtatChanged();
-    partial void OnCommentaireChanging(string value);
-    partial void OnCommentaireChanged();
-    partial void OnDateInterventionChanging(System.DateTime value);
-    partial void OnDateInterventionChanged();
-    partial void OnDefautChanging(int value);
-    partial void OnDefautChanged();
-    partial void OnPersonneChanging(string value);
-    partial void OnPersonneChanged();
-    #endregion
-		
-		public Intervention()
-		{
-			this._Defaut1 = default(EntityRef<Defaut>);
-			this._Personne1 = default(EntityRef<Personne>);
-			OnCreated();
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_IdIntervention", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
-		public int IdIntervention
-		{
-			get
-			{
-				return this._IdIntervention;
-			}
-			set
-			{
-				if ((this._IdIntervention != value))
-				{
-					this.OnIdInterventionChanging(value);
-					this.SendPropertyChanging();
-					this._IdIntervention = value;
-					this.SendPropertyChanged("IdIntervention");
-					this.OnIdInterventionChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Etat", DbType="VarChar(20)")]
-		public string Etat
-		{
-			get
-			{
-				return this._Etat;
-			}
-			set
-			{
-				if ((this._Etat != value))
-				{
-					this.OnEtatChanging(value);
-					this.SendPropertyChanging();
-					this._Etat = value;
-					this.SendPropertyChanged("Etat");
-					this.OnEtatChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Commentaire", DbType="VarChar(500)")]
-		public string Commentaire
-		{
-			get
-			{
-				return this._Commentaire;
-			}
-			set
-			{
-				if ((this._Commentaire != value))
-				{
-					this.OnCommentaireChanging(value);
-					this.SendPropertyChanging();
-					this._Commentaire = value;
-					this.SendPropertyChanged("Commentaire");
-					this.OnCommentaireChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_DateIntervention", DbType="Date")]
-		public System.DateTime DateIntervention
-		{
-			get
-			{
-				return this._DateIntervention;
-			}
-			set
-			{
-				if ((this._DateIntervention != value))
-				{
-					this.OnDateInterventionChanging(value);
-					this.SendPropertyChanging();
-					this._DateIntervention = value;
-					this.SendPropertyChanged("DateIntervention");
-					this.OnDateInterventionChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Defaut", DbType="Int")]
-		public int Defaut
-		{
-			get
-			{
-				return this._Defaut;
-			}
-			set
-			{
-				if ((this._Defaut != value))
-				{
-					if (this._Defaut1.HasLoadedOrAssignedValue)
-					{
-						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
-					}
-					this.OnDefautChanging(value);
-					this.SendPropertyChanging();
-					this._Defaut = value;
-					this.SendPropertyChanged("Defaut");
-					this.OnDefautChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Personne", DbType="VarChar(50)")]
-		public string Personne
-		{
-			get
-			{
-				return this._Personne;
-			}
-			set
-			{
-				if ((this._Personne != value))
-				{
-					if (this._Personne1.HasLoadedOrAssignedValue)
-					{
-						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
-					}
-					this.OnPersonneChanging(value);
-					this.SendPropertyChanging();
-					this._Personne = value;
-					this.SendPropertyChanged("Personne");
-					this.OnPersonneChanged();
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Defaut_Intervention", Storage="_Defaut1", ThisKey="Defaut", OtherKey="IdDefaut", IsForeignKey=true)]
-		public Defaut Defaut1
-		{
-			get
-			{
-				return this._Defaut1.Entity;
-			}
-			set
-			{
-				Defaut previousValue = this._Defaut1.Entity;
-				if (((previousValue != value) 
-							|| (this._Defaut1.HasLoadedOrAssignedValue == false)))
-				{
-					this.SendPropertyChanging();
-					if ((previousValue != null))
-					{
-						this._Defaut1.Entity = null;
-						previousValue.Interventions.Remove(this);
-					}
-					this._Defaut1.Entity = value;
-					if ((value != null))
-					{
-						value.Interventions.Add(this);
-						this._Defaut = value.IdDefaut;
-					}
-					else
-					{
-						this._Defaut = default(int);
-					}
-					this.SendPropertyChanged("Defaut1");
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="Personne_Intervention", Storage="_Personne1", ThisKey="Personne", OtherKey="Mail", IsForeignKey=true)]
-		public Personne Personne1
-		{
-			get
-			{
-				return this._Personne1.Entity;
-			}
-			set
-			{
-				Personne previousValue = this._Personne1.Entity;
-				if (((previousValue != value) 
-							|| (this._Personne1.HasLoadedOrAssignedValue == false)))
-				{
-					this.SendPropertyChanging();
-					if ((previousValue != null))
-					{
-						this._Personne1.Entity = null;
-						previousValue.Interventions.Remove(this);
-					}
-					this._Personne1.Entity = value;
-					if ((value != null))
-					{
-						value.Interventions.Add(this);
-						this._Personne = value.Mail;
-					}
-					else
-					{
-						this._Personne = default(string);
-					}
-					this.SendPropertyChanged("Personne1");
-				}
-			}
-		}
-		
-		public event PropertyChangingEventHandler PropertyChanging;
-		
-		public event PropertyChangedEventHandler PropertyChanged;
-		
-		protected virtual void SendPropertyChanging()
-		{
-			if ((this.PropertyChanging != null))
-			{
-				this.PropertyChanging(this, emptyChangingEventArgs);
-			}
-		}
-		
-		protected virtual void SendPropertyChanged(String propertyName)
-		{
-			if ((this.PropertyChanged != null))
-			{
-				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-			}
 		}
 	}
 }
