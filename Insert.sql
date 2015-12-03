58f8921c54c0a2e63bb347fd84a1ddb80e8fe222
oceaneseel <oceane.seel@hotmail.com>
2015-12-03 22:27:57 +0100

    Insertions en dur

--- Insert.sql
+++ Insert.sql
@@ -1,9 +1,21 @@
 BEGIN
-	INSERT INTO DEFAUTS (PHOTO, DESCRIPTION, POSITION) VALUES (NULL, 'Nid de poule à Verviers',NULL);
+	INSERT INTO DEFAUTS (PHOTO, DESCRIPTION, POSITION) VALUES (NULL, 'Nid de poule à Verviers', '(50.584233;5.896729)');
+	UPDATE DEFAUTS SET Photo = (SELECT BulkColumn FROM Openrowset(Bulk 'C:\Users\Utilisateur\Desktop\projet.Net\Photos\1.jpg', Single_Blob) as Picture) WHERE IDDEFAUT = 1;
+	INSERT INTO DEFAUTS (PHOTO, DESCRIPTION, POSITION) VALUES (NULL, 'Racine d''abre déformant le trottoir à Seraing', '(50.612741;5.509344)');
+	UPDATE DEFAUTS SET PHOTO = (SELECT BulkColumn FROM Openrowset(Bulk 'C:\Users\Utilisateur\Desktop\projet.Net\Photos\2.jpg', Single_Blob) as Picture) WHERE IDDEFAUT = 2;
+	-- LES CHEMINS POUR LES PHOTOS SONT MIS PAR RAPPORT A MON ORDI, JE NE SAIS PAS OU T'AS PLACE TON CLONE DU REPO GIT
 
-	INSERT INTO PERSONNES (MAIL, PASSWORD, NOM, PRENOM, TYPE) VALUES ('oceane.seel@hotmail.com', 'oce', 'SEEL', 'Océane', 'CHEF');
-	INSERT INTO PERSONNES (MAIL, PASSWORD, NOM, PRENOM, TYPE) VALUES ('jerome.fink@hotmail.com', 'je', 'FINK', 'Jérôme', 'OUVRIER');
+	INSERT INTO PERSONNES (MAIL, PASSWORD, NOM, PRENOM, TYPE) VALUES ('oce@gmail.com', 'oce', 'SEEL', 'Océane', 'CHEF');
+	INSERT INTO PERSONNES (MAIL, PASSWORD, NOM, PRENOM, TYPE) VALUES ('je@gmail.com', 'je', 'FINK', 'Jérôme', 'OUVRIER');
 
-	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('OUVERT', 'Problème ouvert', '2015-11-17', 1, 'oceane.seel@hotmail.com');
-	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('EN TRAITEMENT', 'Première approche, analyse du défaut', '2015-11-18', 1, 'jerome.fink@hotmail.com');
+	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('OUVERT', 'Problème ouvert', '2015-11-15', 1, NULL);
+	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('EN TRAITEMENT', 'Assignation d''un ouvrier', '2015-11-15', 1, 'oce@gmail.com');
+	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('A VALIDER', 'Réparations finies, à valider', '2015-11-18', 1, 'je@gmail.com');
+	INSERT INTO INTERVENTIONS (ETAT, COMMENTAIRE, DATEINTERVENTION, DEFAUT, PERSONNE) VALUES('OUVERT', 'Problème ouvert', '2015-11-20', 2, NULL);
+
 END;
+
+
+-- INSERT INTO DEFAUTS(Col1, Col2, Col3, Photo)
+-- SELECT ValCol1, ValCol2, ValCol3, BulkColumn
+-- FROM Openrowset(Bulk 'C:\Users\Utilisateur\Desktop\projet.Net\Photos\1.jpg', Single_Blob) as Picture
