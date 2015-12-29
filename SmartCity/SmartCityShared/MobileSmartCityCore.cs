0778b78e02bcce7301c592b6daef6978637e505c
Jérôme Fink <jeromefink@hotmail.com>
2015-12-29 23:03:43 +0100

    C# ça pue

--- SmartCity/SmartCityShared/MobileSmartCityCore.cs
+++ SmartCity/SmartCityShared/MobileSmartCityCore.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
+
 namespace SmartCityShared
 {
     class MobileSmartCityCore
-    {
+    {  
         public static void sendDefaut()
         {
             Console.WriteLine("yeah");
